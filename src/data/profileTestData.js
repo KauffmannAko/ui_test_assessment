163f1c5ee9b039b182d4526afc12bc7c63e874db
Nana Kauffmann <ako.nana@cm.maviance.com>
2026-04-09 11:01:06 +0100

    added more test in the regression suite

--- src/data/profileTestData.js
+++ src/data/profileTestData.js
@@ -5,5 +5,22 @@ export default {
     email: `john.smith.${Date.now()}@example.com`,
     password: 'P@ssw0rd123',
     confirmPassword: 'P@ssw0rd123',
+    linkedIn: 'https://www.linkedin.com/in/john-smith',
+  },
+  invalidEmailUser: {
+    firstName: 'John',
+    lastName: 'Smith',
+    email: 'john.smith',
+    password: 'P@ssw0rd123',
+    confirmPassword: 'P@ssw0rd123',
+    linkedIn: 'https://www.linkedin.com/in/john-smith',
+  },
+  mismatchedPasswordUser: {
+    firstName: 'John',
+    lastName: 'Smith',
+    email: `john.smith.${Date.now()}+mismatch@example.com`,
+    password: 'Password123',
+    confirmPassword: 'Password456',
+    linkedIn: 'https://www.linkedin.com/in/john-smith',
   },
 };
